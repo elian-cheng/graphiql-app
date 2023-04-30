f06278528abbc14930e0408931b5c3c50b950a1f
Anubic29 <asitailo2039@gmail.com>
2023-04-30 19:31:25 +0300

    init: start graphiql task (create vite react ts app with eslint and prettier)

---
+++ graphiql-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles['block']}>
+      <h1 className={styles['title']}>Graphiql App</h1>
+    </div>
+  );
+}
+
+export default App;
