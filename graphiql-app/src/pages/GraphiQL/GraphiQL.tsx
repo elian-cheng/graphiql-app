eab27bac9ce00421bccd0e105f902e1fe72a6f10
Anubic29 <asitailo2039@gmail.com>
2023-05-07 15:09:01 +0300

    fix: adjust some styles for graphql page

--- graphiql-app/src/pages/GraphiQL/GraphiQL.tsx
+++ graphiql-app/src/pages/GraphiQL/GraphiQL.tsx
@@ -38,63 +38,59 @@ function GraphiQL() {
   }, [dispatch, query, variables, headers]);
 
   return (
-    <Container>
-      <div className={styles['graph-main-block']}>
-        <div className={styles['editor-block']}>
-          <div className={styles['query-block']}>
-            <textarea
-              className={styles['textarea']}
-              value={query}
-              onChange={(event) => dispatch(setQuery(event.target.value))}
-            ></textarea>
-            <div className={styles['command-panel']}>
-              <IconButton className={styles['btn-start']} color="success" onClick={onClickHandler}>
-                <PlayArrow sx={{ fontSize: 20 }} />
-              </IconButton>
-            </div>
-          </div>
-          <Divider />
-          <div className={styles['tab-block']}>
-            <div className={styles['tab-panel']}>
-              <div className={styles['tab']} onClick={() => setTabValue('variables')}>
-                Variables
-              </div>
-              <div className={styles['tab']} onClick={() => setTabValue('headers')}>
-                Headers
-              </div>
-              <div className={styles['btn-close']} onClick={() => setTabValue('')}></div>
-            </div>
-            <div className={styles['tab-window']}>
-              {tabValue === 'variables' && (
-                <div className={styles['textarea-block']}>
-                  <textarea
-                    className={styles['textarea']}
-                    value={variables}
-                    onChange={(event) => dispatch(setVariables(event.target.value))}
-                  ></textarea>
-                </div>
-              )}
-              {tabValue === 'headers' && (
-                <div className={styles['textarea-block']}>
-                  <textarea
-                    className={styles['textarea']}
-                    value={headers}
-                    onChange={(event) => dispatch(setHeaders(event.target.value))}
-                  ></textarea>
-                </div>
-              )}
-            </div>
+    <Container className={styles['graph-main-block']}>
+      <div className={styles['editor-block']}>
+        <div className={styles['query-block']}>
+          <textarea
+            className={styles['textarea']}
+            value={query}
+            onChange={(event) => dispatch(setQuery(event.target.value))}
+          ></textarea>
+          <div className={styles['command-panel']}>
+            <IconButton className={styles['btn-start']} color="success" onClick={onClickHandler}>
+              <PlayArrow sx={{ fontSize: 20 }} />
+            </IconButton>
           </div>
         </div>
-        <div className={styles['response-block']}>
-          {isLoaderGoing ? (
-            <div className={styles['loader-block']}>
-              <CircularProgress />
+        <Divider />
+        <div className={styles['tab-block']}>
+          <div className={styles['tab-panel']}>
+            <div className={styles['tab']} onClick={() => setTabValue('variables')}>
+              Variables
             </div>
-          ) : (
-            <pre>{response}</pre>
+            <div className={styles['tab']} onClick={() => setTabValue('headers')}>
+              Headers
+            </div>
+            <div className={styles['btn-close']} onClick={() => setTabValue('')}></div>
+          </div>
+          {tabValue === 'variables' && (
+            <div className={styles['textarea-block']}>
+              <textarea
+                className={styles['textarea']}
+                value={variables}
+                onChange={(event) => dispatch(setVariables(event.target.value))}
+              ></textarea>
+            </div>
+          )}
+          {tabValue === 'headers' && (
+            <div className={styles['textarea-block']}>
+              <textarea
+                className={styles['textarea']}
+                value={headers}
+                onChange={(event) => dispatch(setHeaders(event.target.value))}
+              ></textarea>
+            </div>
           )}
         </div>
+      </div>
+      <div className={styles['response-block']}>
+        {isLoaderGoing ? (
+          <div className={styles['loader-block']}>
+            <CircularProgress />
+          </div>
+        ) : (
+          <pre>{response}</pre>
+        )}
       </div>
     </Container>
   );
